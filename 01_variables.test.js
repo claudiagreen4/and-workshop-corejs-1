451c55455eaec9fba461dbb31dff533cebffd959
Claudia Green <cgreen@andigital.com>
2017-10-30 10:35:04 +0000

    Exercise 1 - tests

--- 01_variables.test.js
+++ 01_variables.test.js
@@ -1,18 +1,18 @@
 test('VAR', () => {
   var x = 5;
-  var x = 6;
+  // var x = 6;
   expect(x).toBe(5);
 });
 
 test('LET and VAR', () => {
-  var x = 6;
+  // var x = 6;
   let x = 5;
   expect(x).toBe(5);
 });
 
 test('LET', () => {
-  let x = 5;
-  let x = 6;
+  let x;
+  x = 6;
   expect(x).toBe(6);
 });
 
@@ -25,18 +25,18 @@ test('LET', () => {
     return x;
   }
 
-  expect(x).toBe(20);
+  expect(foo()).toBe(20);
 });
 
 test('CONST - scalar values', () => {
   const x = 5;
-  x = 'foo';
+  // x = 'foo';
   expect(x).toBe(5);
 });
 
 test('CONST - assignment', () => {
-  const x;
-  x = 5
+  const x = 5;
+  // x = 5
   expect(x).toBe(5);
 });
 
@@ -45,6 +45,7 @@ test('CONST - objects', () => {
     "name": "Linus",
     "age": 42
   };
+  person.lastname = "torvalds"
   
   expect(person.lastname).toBe('torvalds');
 });
